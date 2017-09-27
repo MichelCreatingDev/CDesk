4358970d66b9ed0a98a29e830fb4260be0bbf196
Michel Stekenlenburg <michelstekelen@gmail.com>
2017-09-27 19:33:06 +0200

    11

--- client/mixin/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-    data: function() {
-        return {
-            baseURL: 'http://139.162.17.230/api',
-            sitekey: '6Le9HSoUAAAAAE27zBFHbXFQdHqBtJrZzLS1v-YJ'
-        }
-    }
-}
